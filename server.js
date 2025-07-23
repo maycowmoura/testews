a5b3f8b485397780ec17352049b41a3aaf60e0f3
maycowmoura <maycowmoura@gmail.com>
2025-07-23 13:58:58 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const WebSocket = require('ws');
+const url = require('url');
+
+const VALID_TOKENS = ['abc123', 'token456'];
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ noServer: true });
+
+const clients = new Map(); // socket => userId
+
+wss.on('connection', (ws, request, userId) => {
+  clients.set(ws, userId);
+  console.log(`Usuário ${userId} conectado`);
+
+  ws.on('message', (msg) => {
+    console.log(`Mensagem de ${userId}:`, msg.toString());
+    ws.send(`Eco: ${msg}`);
+  });
+
+  ws.on('close', () => {
+    clients.delete(ws);
+    console.log(`Usuário ${userId} desconectado`);
+  });
+});
+
+server.on('upgrade', (req, socket, head) => {
+  const { query } = url.parse(req.url, true);
+  const token = query.token;
+
+  if (!VALID_TOKENS.includes(token)) {
+    socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+    socket.destroy();
+    return;
+  }
+
+  const userId = 'user-' + token;
+
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    wss.emit('connection', ws, req, userId);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Servidor WebSocket em http://localhost:3000');
+});
