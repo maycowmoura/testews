cad396f24ed54aeb055be6403a76aebd45e99674
maycowmoura <maycowmoura@gmail.com>
2025-07-23 15:29:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const VALID_TOKENS = ['abc123', 'token456'];
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('ok');
+  res.end((new Date()).toISOString() + ' - Servidor ativo\n');
 });
 const wss = new WebSocket.Server({ noServer: true });
 
