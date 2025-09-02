8cdab6f13ad9a60a49428b3f292db2f8c0673109
maycowmoura <maycowmoura@gmail.com>
2025-09-02 17:02:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,16 @@
 const http = require('http');
-const WebSocket = require('ws');
 const url = require('url');
 const https = require('https');
-process.env.TZ = 'America/Sao_Paulo';
+import aedes from "aedes";
+import ws from "websocket-stream";
 
-const VALID_TOKENS = ['abc123', 'token456'];
+
+const broker = aedes();
+ws.createServer({ server }, broker.handle);
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end((new Date()).toISOString() + ' - Servidor ativo\n');
-});
-const wss = new WebSocket.Server({ noServer: true });
-
-const clients = new Map(); // socket => userId
-
-wss.on('connection', (ws, request, userId) => {
-  clients.set(ws, userId);
-  console.log(`Usuário ${userId} conectado`);
-
-  ws.on('message', (msg) => {
-    console.log(`Mensagem de ${userId}:`, msg.toString());
-    clients.forEach((clientId, client) => {
-        client.send(`${msg}`);
-        console.log(`Enviando para ${clientId}: ${msg}`);
-    })
-  });
-
-  ws.on('close', () => {
-    clients.delete(ws);
-    console.log(`Usuário ${userId} desconectado`);
-  });
-});
-
-server.on('upgrade', (req, socket, head) => {
-  const { query } = url.parse(req.url, true);
-  const token = query.token;
-
-  if (!VALID_TOKENS.includes(token)) {
-    socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-    socket.destroy();
-    return;
-  }
-
-  const userId = 'user-' + token;
-
-  wss.handleUpgrade(req, socket, head, (ws) => {
-    wss.emit('connection', ws, req, userId);
-  });
 });
 
 const HOST = process.env.HOST || '0.0.0.0';
@@ -55,7 +19,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, HOST, () => {
   console.log(`Servidor WebSocket em http://${HOST}:${PORT}`);
 });
-
 
 function doFetch(){
   const url = `https://testews.onrender.com/?${Math.random()}`;
@@ -69,3 +32,7 @@ function doFetch(){
 };
 
 doFetch();
+
+
+
+
